1db0a2ad6262cfde22601829d37ecd53bab1398b
Rain <a.khraisat@xina.tech>
2024-11-03 02:25:24 +0300

    repeated guests analysis and plots utils implemented

--- src/eda/univariate_analysis/__init__.py
+++ src/eda/univariate_analysis/__init__.py
@@ -1,2 +1,3 @@
-from src.eda.univariate_analysis.market_segments_analysis import plot_marketing_segments_insights
 from src.eda.univariate_analysis.cancellation_rates import plot_cancellation_metrics
+from src.eda.univariate_analysis.market_segments_analysis import plot_marketing_segments_insights
+from src.eda.univariate_analysis.repeated_gusests_analysis import plot_returning_customer_analysis
